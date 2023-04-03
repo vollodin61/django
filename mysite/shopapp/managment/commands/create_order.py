b7c6d8eba8e18677862b6ca577e1c4a831552432
ilia_volodin <vollodin61@gmail.com>
2023-04-03 21:00:39 +0300

    На этом коммите я сделал новый проект. Вроде бы всё правильно скопировал, но почему-то страница в браузере отображается криво. Что-то где-то съехало, или что-то где-то я не доделал

---
+++ mysite/shopapp/managment/commands/create_order.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+from django.contrib.auth.models import User
+
+from mysite.shopapp.models import Order
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write('Create Order')
+        user = User.objects.get(username='admin')
+        order = Order.objects.get_or_create(
+            delivery_address='ul Pupkina, d 8',
+            promocode='SALE123',
+            user=user,
+        )
+        self.stdout.write(f'Order created: {order}')
---
+++ mysite/shopapp/managment/commands/update_order.py
@@ -0,0 +1,21 @@
+from django.core.management import BaseCommand
+
+from mysite.shopapp.models import Order, Product
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        order = Order.objects.first()
+        if not order:
+            self.stdout.write("Order not found")
+            return
+        products = Product.objects.all()
+
+        for product in products:
+            order.products.add(product)
+
+        order.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(f"Order {order.products.all()} successfully added to {order}")
+        )
